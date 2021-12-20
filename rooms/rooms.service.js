52d434fa50e8c2d80205de234c8f5845b0676e84
DeVogelRyan <devogelryan2@gmail.com>
2021-12-20 17:22:00 +0100

    first commit

---
+++ rooms/rooms.service.js
@@ -0,0 +1,61 @@
+//Author: De Vogel Ryan
+const connectionPool = require("../config/database");
+
+module.exports = {
+    getAllRooms: (callBack) => {
+        connectionPool.query(
+            'SELECT * FROM Rooms;',
+            (error, results, fields) => {
+                if (error) {
+                    callBack(error);
+                }
+
+                return callBack(null, results);
+            }
+        )
+    },
+    getRoomByID: (id, callBack) => {
+        connectionPool.query(
+            `SELECT * FROM Rooms where id=?`,
+            [id],
+            (error, results, fields) => {
+                if (error) {
+                    callBack(error);
+                }
+                return callBack(null, results);
+            }
+        );
+    },
+    //This is for checking for a duplicate value
+    //source: https://www.titanwolf.org/Network/q/73afed53-5543-4927-bd51-8d4bb977ecf0/y
+    createRoom: (data, callBack) => {
+        connectionPool.query('SELECT * FROM Rooms WHERE code = ?',
+            [data.code],
+            function (err, rows) {
+                if (err) {
+                    callBack(err);
+                }
+                if (!rows.length) {
+                    connectionPool.query(
+                        'INSERT INTO Rooms(id_user,code,open) VALUES (?,?,?)',
+                        [
+                            data.id_user,
+                            data.code,
+                            data.open
+                        ],
+                        (error, results, fields) => {
+                            if (error) {
+                                callBack(error);
+                            }
+
+                            return callBack(null, results);
+                        }
+                    )
+                }
+                else {
+                    console.log("A user posted a duplicate key");
+                    callBack(err);
+                }
+            })
+    },
+}
